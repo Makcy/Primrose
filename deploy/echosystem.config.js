46771bb3090ec74adaf5ddcc8bff17f4dfb60927
Makcy <644215038@qq.com>
2019-05-07 18:23:19 +0800

    fix deploy

--- deploy/echosystem.config.js
+++ deploy/echosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'git@github.com:Makcy/Primrose.git',
       path: '/var/www/production',
-      'post-deploy': 'yarn && pm2 startOrReload deploy/echosystem.config.js --env production',
+      'post-deploy': 'yarn && yarn build && pm2 startOrReload deploy/echosystem.config.js --env production',
       env: {
         NODE_ENV: 'production',
       },
