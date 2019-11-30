b62b4f3e3e1f3036afa00f365591679b7ad20a15
Makcy <644215038@qq.com>
2019-11-30 21:40:21 +0800

    refactor

--- app/containers/index.js
+++ app/containers/index.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {
-  BrowserRouter as Router,
-} from 'react-router-dom';
-import MainPage from '../components/MainPage/MainPage';
+import { view as Todos } from '../components/Todos';
 
 class AppRoot extends Component {
   render() {
     return (
-      <Router>
-        <MainPage />
-      </Router>
+      <div>
+        <Todos />
+      </div>
     );
   }
 }
--- app/index.js
+++ app/index.js
@@ -2,11 +2,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './reducers';
 import AppRoot from './containers';
-
-const store = createStore(rootReducer);
+import store from './store';
 
 // const div = document.createElement('div');
 // div.setAttribute('id', 'app');
